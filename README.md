2c8eb2b8db88e764af3ab1befe66544bb7d03a1e
ha3ks-tech <54911552+ha3ks-tech@users.noreply.github.com>
2019-09-04 20:25:31 +0100

    Intial Commit
    Origional code, works once.. needs more work and some memoey to really make me laugh -- WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+Monty.py(thon) a fun little script to be ran in python
+
+I plan on making this 'smarter' in the future by not allowing the same answer twice in some form of .log however I need to a learn a little more before that happens.
+
+Ive been making this in IntelliJ IDEA Community Edtn.
---
+++ run.py
@@ -0,0 +1,6 @@
+name = raw_input("What is your name? ")  #prompts you to enter your name
+quest = raw_input("What is your quest? ") #prompts you to enter your quest
+color = raw_input("What is your favorite colour? ") #propmts you to enter your favourite colour
+
+print "Ah, so your name is %s, your quest is %s, " \
+      "and your favorite color is %s." % (name, quest, color)
